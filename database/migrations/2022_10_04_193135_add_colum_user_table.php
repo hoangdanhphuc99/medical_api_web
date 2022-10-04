d915fb2c68674cb3bbe17d7e6403ffd2e3b48445
Danh <hoangdanhphuc99@gmail.com>
2022-10-05 03:04:24 +0700

    add colum user

---
+++ database/migrations/2022_10_04_193135_add_colum_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('birthday');
+            $table->text('job');
+            $table->text('address');
+            $table->float('height');
+            $table->text('pathology');
+            $table->integer('sex')->default(0);
+            $table->float('weight');
+            $table->float('service_point');
+            $table->text('other_info');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
