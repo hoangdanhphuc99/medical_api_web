4bf927167ccfe5d9500a77a4cd7df6e9db618870
Nguyen Hong Son <sonnguyencntt04@gmail.com>
2022-10-06 21:20:21 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -30,26 +30,27 @@ Route::group([], function () {
     Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
     Route::post('/check_phone', 'App\Http\Controllers\API\AuthController@checkPhone');
     Route::post('/reset_password', 'App\Http\Controllers\API\AuthController@resetPassword');
+    Route::group(['prefix' => 'posts'], function () {
 
+
+        Route::get('/', 'App\Http\Controllers\API\PostController@index');
+        Route::get('/{id}', 'App\Http\Controllers\API\PostController@show');
+    });
+    Route::group(['prefix' => 'posts_categories'], function () {
+
+
+        Route::get('/', 'App\Http\Controllers\API\ListCategoryController@index');
+        Route::get('/{id}', 'App\Http\Controllers\API\ListCategoryController@show');
+    });
     Route::middleware('auth_user:api')->group(function () {
         Route::get('/profile', 'App\Http\Controllers\API\AuthController@info');
         Route::put('/profile', 'App\Http\Controllers\API\AuthController@updateInfo')->name('user.update');
 
         // Route::apiResource('categories', App\Http\Controllers\API\ListCategoryController::class);
-        Route::group(['prefix' => 'posts_categories'], function () {
 
-
-            Route::get('/', 'App\Http\Controllers\API\ListCategoryController@index');
-            Route::get('/{id}', 'App\Http\Controllers\API\ListCategoryController@show');
-        });
         Route::post('/upload/images', 'App\Http\Controllers\API\UploadController@uploadImg');
         // Route::apiResource('posts', App\Http\Controllers\API\PostController::class);
-        Route::group(['prefix' => 'posts'], function () {
-
-
-            Route::get('/', 'App\Http\Controllers\API\PostController@index');
-            Route::get('/{id}', 'App\Http\Controllers\API\PostController@show');
-        });
+     
     });
 });
 
@@ -64,15 +65,15 @@ Route::group([
     Route::post('/register', 'App\Http\Controllers\API\Admin\AuthController@register');
     Route::post('/reset_password', 'App\Http\Controllers\API\Admin\AuthController@resetPassword');
 
-    Route::apiResource('posts', App\Http\Controllers\API\Admin\PostController::class);
-    Route::apiResource('posts_categories', App\Http\Controllers\API\Admin\ListCategoryController::class);
 
     Route::middleware('auth_admin:api')->group(function () {
         Route::get('/profile', 'App\Http\Controllers\API\Admin\AuthController@info');
         Route::put('/profile', 'App\Http\Controllers\API\Admin\AuthController@updateInfo')->name('user.update');
 
+        Route::apiResource('posts_categories', App\Http\Controllers\API\Admin\ListCategoryController::class);
         Route::post('/upload/images', 'App\Http\Controllers\API\Admin\UploadController@uploadImg');
 
+        Route::apiResource('posts', App\Http\Controllers\API\Admin\PostController::class);
         Route::apiResource('users', App\Http\Controllers\API\Admin\UserController::class);
         Route::apiResource('user_tests', App\Http\Controllers\API\Admin\UserTestController::class);
     });
